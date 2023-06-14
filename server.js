996c0b1438216520f22f941990c8c625aa99d9fe
Mark D. Lalich <mdlalich@gmail.com>
2023-06-14 11:38:05 -0700

    llooks at my artzyness

---
+++ server.js
@@ -0,0 +1,31 @@
+// When a user goes to the /drinks route, they should see an index of all the drinks available at the pub
+// Index: The name of each drink should be rendered to the page, each name should be clickable
+// When a user clicks on the name of one of the drinks, they should be taken to that drink's show page
+// Show: The name, image, and price of each drink should be rendered to the page
+
+
+const express = require("express") 
+const app = express()
+const PORT = (2500)
+
+
+
+// console.log(app) // check one 2....
+
+
+app.get('/drinks', (req,res) => {
+    res.render('index.ejs')
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("The end is nearrrrrr, buy moarrrrr!")
+})
